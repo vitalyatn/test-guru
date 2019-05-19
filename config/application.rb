3c619181550472c517fa359cb673b6afba10c47e
vitalyatn <stolyarov.vitalij@list.ru>
2019-05-19 20:06:12 +1000

    Third version

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module TestGuru
-  VERSION = '0.1.0'
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
