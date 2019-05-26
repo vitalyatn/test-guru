697daf608a0cfa7bc0e898647d0f78461a0b5697
vitalyatn <stolyarov.vitalij@list.ru>
2019-05-26 13:40:24 +1000

    Homework

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,13 @@
 class Test < ApplicationRecord
 
+  belongs_to :category
+  has_many :questions
+
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+
+  belongs_to :author, class_name: "User"
+
   def self.sort_by_desc(category)
     Test.joins("JOIN categories ON categories.title = '#{category}' AND
       categories.id = tests.category_id").order(title: :desc).select(:title)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_085623) do
+ActiveRecord::Schema.define(version: 2019_05_25_114938) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_085623) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "users", force: :cascade do |t|
