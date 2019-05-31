a02e7c0f28f3f8efb5e7c6328c25d6eea6666cdb
vitalyatn <stolyarov.vitalij@list.ru>
2019-05-31 22:43:58 +1000

    Homework

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,49 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: [:new, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:new, :create, :index]
+  before_action :find_question, only: [:show, :destroy, :edit, :update]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render json: {questions: Question.all}
+    @questions = @test.questions
   end
 
   def show
-     render inline: '<strong>Вопрос:</strong> <%= @question.body %>'
+
   end
 
-  def new; end
+  def new
+    @question = Question.new
+  end
 
   def create
-    @question = @test.questions.create(question_params)
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+      if @question.update(question_params)
+        redirect_to @question
+      else
+        render :edit
+      end
   end
 
   def destroy
     @question.destroy
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
   def find_test
+    #byebug
     @test = Test.find(params[:test_id])
   end
 
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,15 @@
 module QuestionsHelper
+
+  def question_header(test_id)
+    Test.find(test_id).title
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def  github_url(author, repo) 
+  link_to 'Project',"https://github.com/#{author}/#{repo}"
+  end
+
 end
