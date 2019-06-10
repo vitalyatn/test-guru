0ff6cbe46f94e02bb6226751a7ec4a90c85ad369
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-10 22:30:37 +1000

    Lesson11

--- db/migrate/20190520122051_add_test_attr_null_constraint.rb
+++ db/migrate/20190520122051_add_test_attr_null_constraint.rb
@@ -1,10 +1,9 @@
 class AddTestAttrNullConstraint < ActiveRecord::Migration[5.2]
   def change
-    change_column_null(:tests, :level, false)
     change_column_null(:tests, :title, false)
     change_column_null(:questions, :body, false)
     change_column_null(:answers, :body, false)
-    change_column_null(:answers, :correct, false)
+
     change_column_null(:users, :name, false)
     change_column_null(:users, :score, false)
   end
