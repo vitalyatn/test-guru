ba2e27159688058b3280b941b6d63d660926b083
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-15 21:31:30 +1000

    Lesson13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,13 @@ module ApplicationHelper
     content_tag :p, flash[key], class: "flash #{key}" if flash[key]
   end
 
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
 
 end
