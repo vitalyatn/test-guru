d7cdb90de0d0558ecdc5868e1105106912f65f2a
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-16 19:58:00 +1000

    lesson13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,17 +7,12 @@ module ApplicationHelper
     link_to 'Project',"https://github.com/#{author}/#{repo}", target: :_blank
   end
 
-  def flash_message(key)
-    content_tag :p, flash[key], class: "flash #{key}" if flash[key]
-  end
-
   def flash_class(level)
-    case level
-      when 'notice' then "alert alert-info"
-      when 'success' then "alert alert-success"
-      when 'error' then "alert alert-danger"
-      when 'alert' then "alert alert-warning"
-    end
+    {
+      notice: 'alert-info',
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning'
+    } [level.to_sym]
   end
-
 end
--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -1,8 +1,10 @@
 class TestsMailer < ApplicationMailer
   def completed_test(test_passage)
-    @user = test_passage.user
-    @test = test_passage.test
+    if test_passage.successful?
+      @user = test_passage.user
+      @test = test_passage.test
 
-    mail to: @user.email
+      mail to: @user.email
+    end
   end
 end
