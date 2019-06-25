86f48a42b37dd0b03e69630db6491d12d5266729
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-25 17:37:45 +1000

    lesson16

---
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -0,0 +1,7 @@
+document.addEventListener('turbolinks:load', function() {
+   var progress = document.querySelector('.progress-bar');
+   var  current_question = progress.dataset.currentQuestion;
+   var total_questions = progress.dataset.totalQuestions;
+   var percent =  current_question / total_questions * 100;
+   progress.setAttribute("style","width:"+ percent +"%")
+})
