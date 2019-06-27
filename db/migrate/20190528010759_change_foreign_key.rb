e67dbfe594daca311b5730b57a8d182fbeaff195
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-27 17:49:20 +1000

    Change

--- db/migrate/20190528010759_change_foreign_key.rb
+++ db/migrate/20190528010759_change_foreign_key.rb
@@ -1,12 +1,20 @@
 class ChangeForeignKey < ActiveRecord::Migration[5.2]
   def change
-    remove_column(:questions, :category_id, :integer)
-    remove_column(:users, :category_id, :integer)
-    remove_column(:tests, :category_id, :integer)
-    remove_column(:answers, :question_id, :integer)
-    remove_column(:questions, :test_id, :integer)
-    remove_column(:passed_tests, :user_id, :integer)
-    remove_column(:passed_tests, :test_id, :integer)
+    # remove_column(:questions, :category_id, :integer)
+    # remove_column(:users, :category_id, :integer)
+    # remove_column(:tests, :category_id, :integer)
+    # remove_column(:answers, :question_id, :integer)
+    # remove_column(:questions, :test_id, :integer)
+    # remove_column(:passed_tests, :user_id, :integer)
+    # remove_column(:passed_tests, :test_id, :integer)
+
+    # remove_column(:questions, :category, :integer)
+    # remove_column(:users, :category_id, :integer)
+    # remove_column(:tests, :category_id, :integer)
+    # remove_column(:answers, :question_id, :integer)
+    # remove_column(:questions, :test_id, :integer)
+    # remove_column(:passed_tests, :user_id, :integer)
+    # remove_column(:passed_tests, :test_id, :integer)
 
     add_reference(:tests, :category, foreign_key: true)
     add_reference(:answers, :question, foreign_key: true)
