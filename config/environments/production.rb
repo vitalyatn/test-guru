036565af7008477c93c37e5152489ca406572576
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-27 21:07:22 +1000

    h

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,16 +63,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://glacial-harbor-90375.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true }
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'https://glacial-harbor-90375.herokuapp.com/' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: ENV['SMTP_USERNAME'],
+  #   password: ENV['SMTP_PASSWORD'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
