b78a0f46183c4244a49cf43e585dd7e040459d93
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-27 21:25:46 +1000

    actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'still-cove-48658.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
     enable_starttls_auto: true }
 
 
