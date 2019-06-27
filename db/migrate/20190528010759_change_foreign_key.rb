1fc2b6188403c281283a8b346f50876dea5ac469
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-27 18:47:05 +1000

    migrate

--- db/migrate/20190528010759_change_foreign_key.rb
+++ db/migrate/20190528010759_change_foreign_key.rb
@@ -9,7 +9,7 @@ class ChangeForeignKey < ActiveRecord::Migration[5.2]
     # remove_column(:passed_tests, :test_id, :integer)
 
     #remove_column(:questions, :category, :integer)
-    remove_column(:users, :category_id, :integer)
+    #remove_column(:users, :category_id, :integer)
     remove_column(:tests, :category_id, :integer)
     remove_column(:answers, :question_id, :integer)
     remove_column(:questions, :test_id, :integer)
