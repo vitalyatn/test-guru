48c92c653822de77cda0b694da93bd8dd6112d7c
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-29 18:53:40 +1000

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,16 +74,7 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true }
 
-  # 
-  # config.action_mailer.smtp_settings = {
-  #     :user_name => 'stolyarov.vitalij',
-  #     :password => '12189741a',
-  #   #  :domain => 'heroku.com',
-  #     :address => 'smtp.sendgrid.net',
-  #     :port => 587,
-  #     :authentication => :plain,
-  #     :enable_starttls_auto => true
-  #   }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
