bb16867e61e875c313450eb503518ba1c1d5ea2a
vitalyatn <stolyarov.vitalij@list.ru>
2019-06-29 19:11:35 +1000

    lesson17

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,14 +1,11 @@
 class FeedbackMailer < ApplicationMailer
 
   before_action { @mailer = params[:email] }
-
   #default from: @mailer
-
   def send_feedback(feedback)
     @user = 'supervitas28@gmail.com'
     @body = feedback.body
     mail to: @user, from: @mailer
-    #byebug
   end
 
 end
