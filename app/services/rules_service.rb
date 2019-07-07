5472b416ed192066ae4802dd3a712569a610fd4e
vitalyatn <stolyarov.vitalij@list.ru>
2019-07-07 21:18:56 +1000

    v3

--- app/services/rules_service.rb
+++ app/services/rules_service.rb
@@ -6,36 +6,44 @@ class RulesService
   end
 
   def give_bage
-    Bage.select { |badge| send("rule_#{badge.rule}?", badge.value) }
+    Bage.select { |bage| send("rule_#{bage.rule}?", bage) }
   end
-
 
   private
 
-  def rule_all_category_completed?(category_id)
-    list_tests_general= Test.select { |test| test.category.id == category_id.to_i }
-    list_user_tests = @current_user.tests.select {|test| test.category.id == category_id.to_i}
-    (list_user_tests - list_tests_general).empty?
+  def rule_all_category_completed?(bage)
+    category_id = bage.value
+    return check_category_and_successful?(category_id.to_i)
+    list_tests_general = Test.where(category_id: category_id)
+    list_user_tests = @current_user.tests.where(category_id: category_id)
+    if (list_user_tests - list_tests_general).empty?
+      list_tests_general.pluck(:id) == list_user_tests.pluck(:id).uniq &&
+        list_user_tests.pluck(:id).group_by(&:itself).values.map(&:size).uniq.size == 1
+    end
   end
 
-  def rule_first_attempt?(value)
+  def rule_first_attempt?(bage)
     @current_user.test_passages.where(test_id: @test_passage.test).count == 1 && @test_passage.successful
   end
 
-  def rule_first_test_completed?(category_id)
+  def rule_first_test_completed?(bage)
+    category_id = bage.value
+    return check_category_and_successful?(category_id.to_i)
     @current_user.tests.where(category_id: category_id).count == 1 &&
     @test_passage.test.category_id == category_id && @test_passage.successful
   end
 
-  def rule_three_last_successful?(value)
-   tests = @current_user.test_passages.order(created_at: :desc)[0..2]
-   return false unless tests.all?{ |test| test.successful == true }
-   return false if tests.pluck(:test_id).uniq.length != 3
-   #tests_id = tests.pluck(:test_id).uniq if tests.pluck(:test_id).uniq.length == 3
-   a = @current_user.test_passages.where(test: tests[0].test_id, successful: true).count
-   b = @current_user.test_passages.where(test: tests[1].test_id, successful: true).count
-   c = @current_user.test_passages.where(test: tests[2].test_id, successful: true).count
-   [a, b, c].uniq.size == 1
+  def rule_three_last_successful?(bage)
+    last_date = ReceivedBage.where(user: @current_user, bage: bage).order(created_at: :desc).pluck(:created_at).first ||
+    @current_user.test_passages.order(created_at: :asc).pluck(:created_at).first
+    passed_tests = @current_user.test_passages.where(created_at: last_date.utc..Time.current.utc)
+    tests_succesfull = passed_tests.order(created_at: :desc)[0..2].pluck(:successful)
+    tests_uniq = passed_tests.order(created_at: :desc)[0..2].pluck(:test_id).uniq
+    tests_succesfull.all? && tests_succesfull.size == 3 && tests_uniq.size == 3
+  end
+
+  def check_category_and_successful?(category_id)
+    category_id != @test_passage.test.category_id  && !@test_passage.successful
   end
 
 end
