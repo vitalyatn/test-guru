f8774bcdd487ec04d33a7110c769e6b55c4dce9a
vitalyatn <stolyarov.vitalij@list.ru>
2019-07-07 23:50:50 +1000

    v4

--- app/services/rules_service.rb
+++ app/services/rules_service.rb
@@ -12,13 +12,13 @@ class RulesService
   private
 
   def rule_all_category_completed?(bage)
-    category_id = bage.value
-    return check_category_and_successful?(category_id.to_i)
-    list_tests_general = Test.where(category_id: category_id)
-    list_user_tests = @current_user.tests.where(category_id: category_id)
-    if (list_user_tests - list_tests_general).empty?
-      list_tests_general.pluck(:id) == list_user_tests.pluck(:id).uniq &&
-        list_user_tests.pluck(:id).group_by(&:itself).values.map(&:size).uniq.size == 1
+    category_id = bage.value.to_i
+    return if check_category_and_successful?(category_id)
+    category_tests = Test.where(category_id: category_id)
+    user_tests = @current_user.test_passages.where(successful: true).joins(:test).where(tests: {category: category_id })
+    if (user_tests - category_tests).empty?
+      category_tests.pluck(:id) == user_tests.pluck(:id).uniq &&
+        user_tests.pluck(:id).group_by(&:itself).values.map(&:size).uniq.size == 1
     end
   end
 
@@ -27,8 +27,8 @@ class RulesService
   end
 
   def rule_first_test_completed?(bage)
-    category_id = bage.value
-    return check_category_and_successful?(category_id.to_i)
+    category_id = bage.value.to_i
+    return if check_category_and_successful?(category_id)
     @current_user.tests.where(category_id: category_id).count == 1 &&
     @test_passage.test.category_id == category_id && @test_passage.successful
   end
@@ -37,13 +37,13 @@ class RulesService
     last_date = ReceivedBage.where(user: @current_user, bage: bage).order(created_at: :desc).pluck(:created_at).first ||
     @current_user.test_passages.order(created_at: :asc).pluck(:created_at).first
     passed_tests = @current_user.test_passages.where(created_at: last_date.utc..Time.current.utc)
-    tests_succesfull = passed_tests.order(created_at: :desc)[0..2].pluck(:successful)
-    tests_uniq = passed_tests.order(created_at: :desc)[0..2].pluck(:test_id).uniq
-    tests_succesfull.all? && tests_succesfull.size == 3 && tests_uniq.size == 3
+    tests_succesfull = passed_tests.order(created_at: :desc).first(3).select(&:successful)
+    tests_uniq = passed_tests.order(created_at: :desc).first(3).pluck(:test_id).uniq
+    tests_succesfull.size == 3 && tests_uniq.size == 3
   end
 
   def check_category_and_successful?(category_id)
-    category_id != @test_passage.test.category_id  && !@test_passage.successful
+    category_id != @test_passage.test.category_id  || !@test_passage.successful
   end
 
 end
